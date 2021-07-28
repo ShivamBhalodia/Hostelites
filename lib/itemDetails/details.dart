7f987ca6e11856fe76727480cd4fe1f1435656a0
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-07-28 19:12:38 +0530

    Details Screen Updated

--- lib/itemDetails/details.dart
+++ lib/itemDetails/details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hostel_app/utils/size_config.dart';
 
 class MealDetailScreen extends StatefulWidget {
   MealDetailScreen();
@@ -12,7 +11,6 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    print(size);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -20,7 +18,7 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
         elevation: 0,
         leading: IconButton(
           icon: Icon(
-            Icons.assistant_direction_sharp,
+            Icons.arrow_back,
             color: Colors.white,
           ),
           onPressed: () {},
@@ -41,43 +39,34 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
             semanticContainer: true,
             clipBehavior: Clip.antiAliasWithSaveLayer,
             child: Image.asset("assets/images/lunch.jpeg",
-                fit: BoxFit.cover,
-                width: 89.9305555556 * SizeConfig.widthMultiplier,
-                height: 31.9989834816 * SizeConfig.heightMultiplier),
+                fit: BoxFit.cover, width: 370, height: size.height * 0.32),
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                  4.44726810673 * SizeConfig.heightMultiplier),
+              borderRadius: BorderRadius.circular(40.0),
             ),
             elevation: 6,
-            margin: EdgeInsets.symmetric(
-                horizontal: 4.86111111111 * SizeConfig.widthMultiplier,
-                vertical: 1.66772554003 * SizeConfig.heightMultiplier),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
           ),
           Container(
-            padding: EdgeInsets.symmetric(
-                horizontal: 4.86111111111 * SizeConfig.widthMultiplier),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             width: double.infinity,
             child: Column(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 1.45833333333 * SizeConfig.widthMultiplier),
+                  padding: EdgeInsets.symmetric(horizontal: 6),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       Text(
                         'Piza',
                         style: TextStyle(
-                            fontSize:
-                                2.22363405337 * SizeConfig.heightMultiplier,
+                            fontSize: 20,
                             color: Color(0xFF3a3a3b),
                             fontWeight: FontWeight.w500),
                       ),
                       Text(
                         '\$100',
                         style: TextStyle(
-                            fontSize:
-                                2.22363405337 * SizeConfig.heightMultiplier,
+                            fontSize: 20,
                             color: Color(0xFF3a3a3b),
                             fontWeight: FontWeight.w500),
                       ),
@@ -85,7 +74,7 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
                   ),
                 ),
                 SizedBox(
-                  height: 0.55590851334 * SizeConfig.heightMultiplier,
+                  height: 5,
                 ),
                 Row(
                   children: <Widget>[
@@ -93,17 +82,16 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
                       Icons.location_on,
                       color: Colors.yellow,
                     ),
-                    SizedBox(width: 2.43055555556 * SizeConfig.widthMultiplier),
+                    SizedBox(width: 10),
                     Text('Burger king'),
                   ],
                 ),
                 Padding(
-                  padding: EdgeInsets.only(
-                      top: 2.22363405337 * SizeConfig.heightMultiplier),
+                  padding: EdgeInsets.only(top: 20),
                   child: Text(
                     'Description',
                     style: TextStyle(
-                        fontSize: 2.22363405337 * SizeConfig.heightMultiplier,
+                        fontSize: 20,
                         color: Color(0xFF3a3a3b),
                         fontWeight: FontWeight.w500),
                   ),
@@ -113,16 +101,12 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
           ),
           Expanded(
               child: Container(
-                  margin: EdgeInsets.symmetric(
-                      horizontal: 4.86111111111 * SizeConfig.widthMultiplier,
-                      vertical: 1.11181702668 * SizeConfig.heightMultiplier),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   child: SingleChildScrollView(
                       child: Text(
                           'What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has? What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?')))),
           Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: 4.86111111111 * SizeConfig.widthMultiplier,
-                vertical: 1.11181702668 * SizeConfig.heightMultiplier),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -132,25 +116,23 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
                       onPressed: () {},
                       icon: Icon(Icons.remove),
                       color: Colors.red,
-                      iconSize: 2.22363405337 * SizeConfig.heightMultiplier,
+                      iconSize: 20,
                     ),
                     InkWell(
                       onTap: () {},
                       child: Container(
-                        width: 12.1527777778 * SizeConfig.widthMultiplier,
-                        height: 3.33545108005 * SizeConfig.heightMultiplier,
+                        width: 50.0,
+                        height: 30.0,
                         decoration: BoxDecoration(
                           color: Colors.red,
                           border: Border.all(color: Colors.white, width: 0.0),
-                          borderRadius: BorderRadius.circular(
-                              1.11181702668 * SizeConfig.heightMultiplier),
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                         child: Center(
                           child: Text(
                             '1',
                             style: TextStyle(
-                                fontSize:
-                                    2.00127064803 * SizeConfig.heightMultiplier,
+                                fontSize: 18.0,
                                 color: Colors.white,
                                 fontWeight: FontWeight.w400),
                           ),
@@ -161,7 +143,7 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
                       onPressed: () {},
                       icon: Icon(Icons.add),
                       color: Colors.red,
-                      iconSize: 2.22363405337 * SizeConfig.heightMultiplier,
+                      iconSize: 20,
                     ),
                   ],
                 ),
@@ -169,7 +151,7 @@ class _MealDetailScreenState extends State<MealDetailScreen> {
                   onPressed: () {},
                   icon: Icon(Icons.bookmark_add_outlined),
                   color: Color(0xFFfd2c2c),
-                  iconSize: 3.33545108005 * SizeConfig.heightMultiplier,
+                  iconSize: 30,
                 ),
               ],
             ),
