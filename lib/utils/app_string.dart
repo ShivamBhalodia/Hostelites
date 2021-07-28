2b7854e04394152e3f85ace03984c3ec0ee41a5a
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-07-28 19:18:21 +0530

    Files For Responsive UI Added

---
+++ lib/utils/app_string.dart
@@ -0,0 +1,11 @@
+class AString {
+  AString._();
+
+  static const String bottomNavigationHome = "Home";
+  static const String bottomNavigationPastOrder = "Your Orders";
+  static const String bottomNavigationYourOrder = "Your Cart";
+  static const String comingsoon = "Coming Soon";
+  static const String image1 =
+      'https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
+  static const String address1 = "Kanal Road, Near Vardhman, Morbi-363641 nscjncjsncsc  scwbwc scschcjcj dcj sv jehvbevj djhs vjbvjevj j jbv";
+}
