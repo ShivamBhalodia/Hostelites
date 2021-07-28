fe296bd81a6d1d9e58e199d35ff6425f312d4c9c
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-07-28 19:24:13 +0530

    About Us Added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'cart/cart.dart';
 import 'itemDetails/details.dart';
 import 'editProfile/editProfile.dart';
 import './itemList/itemList.dart';
+import './aboutUs.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ProfileScreen(),
+      home: AboutUs(),
     );
   }
 }
