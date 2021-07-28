1da4fa58aa5fe94371b7a56cefa0e0244a0fedf2
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-07-28 19:20:12 +0530

    User Model For Profile

---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+class UserModel {
+  final String userName, userEmail, userPhoneNumber, userImage, userAddress;
+  const UserModel(
+      {required this.userEmail,
+      required this.userImage,
+      required this.userAddress,
+      required this.userName,
+      required this.userPhoneNumber});
+}
