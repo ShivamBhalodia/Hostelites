8a0c01bebcecd2e1f51a40f23f3d50646a727e79
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-08-02 19:02:23 +0530

    Model for places and search places done

---
+++ lib/models/allnearbyPlaces.dart
@@ -0,0 +1,17 @@
+import 'package:hostel_app/models/place.dart';
+
+class AllNearbyPlaces {
+  final String icon;
+  final String name;
+  final String vicinity;
+
+  AllNearbyPlaces({this.icon, this.name, this.vicinity});
+
+  factory AllNearbyPlaces.fromJson(Map<String, dynamic> json) {
+    return AllNearbyPlaces(
+      icon: json['icon'],
+      name: json['name'],
+      vicinity: json['vicinity'],
+    );
+  }
+}
