a7b69e936f304ac20118f0036a53d3e0eb035a92
ShivamBhalodia <shivambhalodia007@gmail.com>
2021-08-07 23:06:54 +0530

    Providers For Shopkeeper and Consumer

---
+++ lib/providers/p_consumer.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class P_Consumer with ChangeNotifier {
+  Future<void> registerConsumer(
+    String phone,
+    String password,
+    String confirmPassword,
+    String name,
+    bool isUser,
+  ) async {
+    final url = 'http://953fa7d8445a.ngrok.io/validatephonesendotp';
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(
+          {
+            'shopkeeper': isUser,
+            'phone': phone,
+            'name': name,
+            'password': password,
+            'password2': confirmPassword,
+          },
+        ),
+      );
+      final res = json.decode(response.body);
+      print("jainam in p_consumer");
+      print(res);
+      if (res['status'] != true) {
+        throw HttpException(res['detail']);
+      }
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+}
